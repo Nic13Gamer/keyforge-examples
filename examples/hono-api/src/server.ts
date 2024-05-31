f99b9a70f00f81d17ad98698108fd250a4718de5
Nic13Gamer <54724556+Nic13Gamer@users.noreply.github.com>
2024-05-31 18:44:01 -0300

    feat(hono): download endpoint

--- examples/hono-api/src/server.ts
+++ examples/hono-api/src/server.ts
@@ -1,10 +1,39 @@
+import 'dotenv/config';
+
 import { serve } from '@hono/node-server';
 import { Hono } from 'hono';
+import { Keyforge } from 'keyforge-js';
+
+const API_KEY = process.env.KEYFORGE_API_KEY!;
+const PRODUCT_ID = process.env.KEYFORGE_PRODUCT_ID!;
 
 const app = new Hono();
+const keyforge = new Keyforge(API_KEY);
 
-app.get('/', (c) => {
-  return c.text('Hello World!');
+app.get('/download', async (c) => {
+  const licenseKey = c.req.query('license_key');
+
+  if (!licenseKey) {
+    c.status(400);
+
+    return c.text(
+      'License key is required. Use the "license_key" query parameter to specify it.'
+    );
+  }
+
+  const { isValid } = await keyforge.licenses.validate(licenseKey, {
+    productId: PRODUCT_ID,
+  });
+
+  if (!isValid) {
+    c.status(400);
+
+    return c.text('Invalid license key.');
+  }
+
+  return c.text(
+    "Thank you for buying our product! Here's the download link: <YOUR_DOWNLOAD_LINK>."
+  );
 });
 
 serve(
