50ed44e51dc397b2cf0186bd7a7c93d9f1bdc201
Nic13Gamer <54724556+Nic13Gamer@users.noreply.github.com>
2024-05-31 17:01:52 -0300

    setup Hono example

---
+++ examples/hono-api/README.md
@@ -0,0 +1,3 @@
+# Hono API
+
+TODO...
--- examples/hono-api/src/server.ts
+++ examples/hono-api/src/server.ts
@@ -1,1 +1,16 @@
-console.log('Hello World!');
+import { serve } from '@hono/node-server';
+import { Hono } from 'hono';
+
+const app = new Hono();
+
+app.get('/', (c) => {
+  return c.text('Hello World!');
+});
+
+serve(
+  {
+    fetch: app.fetch,
+    port: 8787,
+  },
+  () => console.log('Server is running on http://localhost:8787')
+);
